a573b753e5cc329d567a3cd4fd7d095c9de48537
NicholasNgaruiya <nicholasngaruiya387@gmail.com>
2023-10-04 12:23:38 +0300

    created a product tile widget to display each product dynamically

--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grocery_shop/features/home/ui/product_tile_widget.dart';
 
 import '../../cart/ui/cart.dart';
 import '../../wishlist/ui/wishlist.dart';
@@ -42,26 +43,33 @@ class _HomeState extends State<Home> {
               body: Center(child: CircularProgressIndicator()),
             );
           case HomeLoadedSuccessState:
+            final successState = state as HomeLoadedSuccessState;
             return Scaffold(
-              appBar: AppBar(
-                title: const Text("Grocery App"),
-                backgroundColor: Colors.teal,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      homeBloc.add(HomeWishlistButtonNavigateEvent());
-                    },
-                    icon: const Icon(Icons.favorite_outline),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      homeBloc.add(HomeCartButtonNavigateEvent());
-                    },
-                    icon: const Icon(Icons.shopping_cart_outlined),
-                  ),
-                ],
-              ),
-            );
+                appBar: AppBar(
+                  title: const Text("Grocery App"),
+                  backgroundColor: Colors.teal,
+                  actions: [
+                    IconButton(
+                      onPressed: () {
+                        homeBloc.add(HomeWishlistButtonNavigateEvent());
+                      },
+                      icon: const Icon(Icons.favorite_outline),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        homeBloc.add(HomeCartButtonNavigateEvent());
+                      },
+                      icon: const Icon(Icons.shopping_cart_outlined),
+                    ),
+                  ],
+                ),
+                body: ListView.builder(
+                  itemCount: successState.products.length,
+                  itemBuilder: ((context, index) {
+                    return ProductTileWidget(
+                        productDataModel: successState.products[index]);
+                  }),
+                ));
           case HomeErrorState:
             return const Scaffold(
               body: Center(child: Text("Error")),
